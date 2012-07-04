5167ab8b4ec12e21951eb715f340f19e48db676e
Bill Wolfe <wwolfe@gmail.com>
2012-07-04 02:08:00 -0400

    Printer Configuration: Create the lazy loaded printer configuration object that is loaded from the database.

--- src/com/floreantpos/model/PrinterConfiguration.java
+++ src/com/floreantpos/model/PrinterConfiguration.java
@@ -1,39 +1,60 @@
 package com.floreantpos.model;
 
+import com.floreantpos.dal.PosSessionFactory;
 import com.floreantpos.model.base.BasePrinterConfiguration;
-
+import org.hibernate.Session;
 
 
 public class PrinterConfiguration extends BasePrinterConfiguration {
 	private static final long serialVersionUID = 1L;
 
-/*[CONSTRUCTOR MARKER BEGIN]*/
-	public PrinterConfiguration () {
+	private static PrinterConfiguration printerConfiguration;
+
+	/*[CONSTRUCTOR MARKER BEGIN]*/
+	public PrinterConfiguration() {
 		super();
 	}
 
 	/**
 	 * Constructor for primary key
 	 */
-	public PrinterConfiguration (java.lang.Integer id) {
+	public PrinterConfiguration(java.lang.Integer id) {
 		super(id);
 	}
 
 /*[CONSTRUCTOR MARKER END]*/
 
 	public final static Integer ID = Integer.valueOf(1);
-	
+
+	//Lazy loaded copy of the current print settings
+	public static PrinterConfiguration printerConfiguration() {
+		//Null? go ahead and load from the DB
+		if (printerConfiguration == null) {
+			Session currentSession = PosSessionFactory.currentSession();
+
+			printerConfiguration = (PrinterConfiguration) currentSession.get(PrinterConfiguration.class, 1);
+			if (printerConfiguration == null) {
+				//still null? don't have one in the DB yet!
+				currentSession.beginTransaction();
+				printerConfiguration = new PrinterConfiguration(1);
+				currentSession.save("PrinterConfiguration", printerConfiguration);
+				currentSession.getTransaction().commit();
+			}
+		}
+		return printerConfiguration;
+	}
+
 	@Override
 	public String getReceiptPrinterName() {
-		if(super.getReceiptPrinterName() == null) {
+		if (super.getReceiptPrinterName() == null) {
 			return "PosPrinter";
 		}
 		return super.getReceiptPrinterName();
 	}
-	
+
 	@Override
 	public String getKitchenPrinterName() {
-		if(super.getKitchenPrinterName() == null) {
+		if (super.getKitchenPrinterName() == null) {
 			return "KitchenPrinter";
 		}
 		return super.getKitchenPrinterName();
